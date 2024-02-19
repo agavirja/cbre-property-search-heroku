24ce7a334a7eee41edf221fc58bf668a83c43409
agavirja <32424558+agavirja@users.noreply.github.com>
2024-02-18 19:43:39 -0500

    actualizar app

--- pages/2_Analisis_de_predios.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from _analisis_individual import main
-
-st.set_page_config(layout='wide',initial_sidebar_state="collapsed",page_icon ="https://www.cbre.com/-/media/project/cbre/dotcom/global/unsorted/favicon_lg.png?rev=f6bed35a1dfb4fac9ede077cef213618")
-
-
-main()
